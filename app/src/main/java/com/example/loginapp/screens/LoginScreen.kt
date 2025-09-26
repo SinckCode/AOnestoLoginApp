6fae8f4406599814274b15a724dbaa26d3e9fcc1
SinckCode <soyangeldavid1@gmail.com>
2025-09-26 10:54:28 -0600

    Separacion de columns

--- app/src/main/java/com/example/loginapp/screens/LoginScreen.kt
+++ app/src/main/java/com/example/loginapp/screens/LoginScreen.kt
@@ -6,24 +6,54 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun LoginScreen(
     navController : NavController,
     innerPadding : PaddingValues
 ){
-    Row(
+    Column(
         modifier = Modifier
-            .fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Login Screen")
-        Button(onClick = {navController.navigate(RegisterScreenRoute)}) { }
+
+
+        Column(modifier = Modifier
+            .weight(1f)) {
+            Text(text = "Login Screen")
+            Button(onClick = {navController.navigate(RegisterScreenRoute)}) { }
+            Text(text = "hola")
+        }
+
+        Column(modifier = Modifier
+            .weight(1f)) {
+            Text(text = "Login Screen")
+            Button(onClick = {navController.navigate(RegisterScreenRoute)}) { }
+            Text(text = "hola")
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun LoginScreenPreview() {
+    MaterialTheme {
+        LoginScreen(
+            navController = rememberNavController(),
+            innerPadding = PaddingValues(0.dp)
+        )
     }
 }
